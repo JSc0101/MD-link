0916dc4d336fb46cb26ef96cd3e59e4b33c383bc
Sebastian009w <ia3266505@gmail.com>
2023-02-14 11:24:03 -0500

    documentacion

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
 import { pathnameExist } from "./validator";
 
-export const mdLink = (path: string, options: string) => {
+/**
+ *
+ * @param path | este parametro recibe la ruta a evaluar
+ * @param options |  un array de opciones de comando
+ * @returns | mi funcion retorna una promesa
+ * ! es indipensable que la funcion mdLink lleve sus dos parametros
+ */
+export const mdLink = (path: string, options: Array<string>) => {
+  /**
+   * @param resolve | es lo que debe resolverse
+   * @param reject | rechaza si la promesa no se cumple
+   */
   return new Promise((resolve, reject) => {
-    if (options === undefined) {
+    /**
+     * ! options[] no puede estar undefined
+     */
+    if (options[0] === undefined && options[1] === undefined) {
       reject("error ruta undefined");
-    } else {
-      pathnameExist(path);
-      resolve("is valid");
     }
+    console.log(`${resolve('very so good')} ${path}`)
   });
 };
