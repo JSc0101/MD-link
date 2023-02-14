46a3b531af3071c282d58b78c4599f79156acfae
Sebastian009w <ia3266505@gmail.com>
2023-02-14 11:34:11 -0500

    se genera documentacion con tsDoc

--- validator.ts
+++ validator.ts
@@ -1,12 +1,22 @@
 import { existsSync } from "fs";
 import { isAbsolute } from "path";
 
+/**
+ *
+ * @param pathname | es la ruta
+ * @returns validar si existe una path y devuelve un boolean
+ */
 const pathnameExist = (pathname: string): boolean => {
   const isValid = existsSync(pathname);
   return isValid ? true : false;
 };
 
 
+/**
+ *
+ * @param pathname es la ruta
+ * @returns validar si la ruta es absoluta
+ */
 const validatorAbslute = (pathname: string):boolean => {
   const absolute = isAbsolute(pathname);
   return absolute ? true : false
