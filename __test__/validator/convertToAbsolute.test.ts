16123b619cce1c09189a7a833b45a28bcc688838
Sebastian009w <ia3266505@gmail.com>
2023-02-21 14:54:21 -0500

    se inicio el testing de la funcion mdLInk

--- __test__/validator/convertToAbsolute.test.ts
+++ __test__/validator/convertToAbsolute.test.ts
@@ -3,10 +3,6 @@ import { pathRelative } from "./pathname";
 
 describe("function converToAbsolute", () => {
   describe("the converToAbsolute function test", () => {
-    test("should be a function", () => {
-      expect(typeof converToAbsolute).toEqual("function");
-    });
-
     test("should return an path absolute", () => {
       expect(converToAbsolute(pathRelative)).toContain(
         "C:\\Users\\NICOLAS\\Desktop\\MD-Link\\__test__\\files\\others\\testing.md"
