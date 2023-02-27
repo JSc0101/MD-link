14c31a67e745d7febbc23cc5b19a16f876ec3b45
Sebastian009w <ia3266505@gmail.com>
2023-02-26 23:07:49 -0500

    se valida las opciones de la f uncion mdLInks

--- cli.ts
+++ cli.ts
@@ -1,8 +1,6 @@
 import { mdLink } from "./index";
 
-mdLink("C:/Users/NICOLAS/Desktop/MD-Link/__test__/files/helloWorld.m", [
-  "--validate",
-])
+mdLink("README.md", ["--validate"])
   .then((path) => {
     console.log(path);
   })
